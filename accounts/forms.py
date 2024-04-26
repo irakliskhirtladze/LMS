fd573ea00c5d66c72c3633e5d98f57e204fd73de
NikaNikolozi <NikaNozadze166@gmail.com>
2024-04-26 22:20:30 +0400

    Lector page modification

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from accounts.models import CustomUser
+from django import forms
+from .models import Homework
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -13,3 +15,9 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('email',)
+
+
+class Work(forms.ModelForm):
+    class Meta:
+        model = Homework
+        fields = ['description', 'due_date']
